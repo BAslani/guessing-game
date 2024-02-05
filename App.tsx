af68fbebecbc454269bfdb53684889f1940d38ec
Behzad Aslani <behzadaslani80@gmail.com>
2024-02-05 12:46:41 +0330

    feat: added background layout

--- App.tsx
+++ App.tsx
@@ -1,10 +1,32 @@
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, ImageBackground } from "react-native"
 import StartGameScreen from "./screens/StartGameScreen"
+import { LinearGradient } from "expo-linear-gradient"
 
 const App = () => {
-  return <StartGameScreen />
+  return (
+    <LinearGradient
+      colors={["#4e0329", "#ddb52f"]}
+      style={styles.rootBackground}
+    >
+      <ImageBackground
+        source={require("./assets/images/background.png")}
+        resizeMode='cover'
+        style={styles.rootBackground}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
+  )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  rootBackground: {
+    flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
+  },
+})
 
 export default App
